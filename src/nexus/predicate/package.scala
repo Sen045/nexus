18a0807e1c1f59d2540a998a98deadf71f0223b6
Sen045 <none specified>
2012-12-17 23:21:42 +0100

    package objects to generate documentation

---
+++ src/nexus/predicate/package.scala
@@ -0,0 +1,20 @@
+/** This file is part of Nexus, which is Copyright 2012 Johannes Åman Pohjola.
+ *
+ *  Nexus is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  Nexus is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Nexus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nexus
+
+/** A collection of classes related to representing and parsing predicates
+ *  on strings.
+ */
+package object predicate
